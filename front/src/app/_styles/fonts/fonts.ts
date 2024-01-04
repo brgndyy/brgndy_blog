88a76fc0f44a15497d2ee10c9775257b2d42db3e
brgndy <brgndy96@gmail.com>
2024-01-04 12:49:39 +0900

    Style: 폰트 선언

---
+++ front/src/app/_styles/fonts/fonts.ts
@@ -0,0 +1,6 @@
+import localFont from 'next/font/local';
+
+const BMHAANAPro = localFont({ src: '/BMHANNAPro.ttf' });
+const BMHANNAAir = localFont({ src: '/BMHANNAAir_ttf.ttf' });
+
+export { BMHAANAPro, BMHANNAAir };
