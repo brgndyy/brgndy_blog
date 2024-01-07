1d7ff789a519ae1f24f026a1cccfbf5d31698724
brgndy <brgndy96@gmail.com>
2024-01-07 11:01:34 +0900

    Refactor: 공용 Wrapper 컴포넌트 Container로 교체

--- front/src/app/_components/_admin/AdminMenu.tsx
+++ front/src/app/_components/_admin/AdminMenu.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ADMIN_MENU_LIST from '@/app/_constants/adminMenuList';
 import { menuContainer } from './adminMenu.css';
 import AdminLink from './AdminLink';
-import Wrapper from '../_composables/wrapper/Wrapper';
+import Container from '../_composables/container/Container';
 
 export default function AdminMenu() {
   return (
-    <Wrapper className={menuContainer}>
+    <Container className={menuContainer}>
       {ADMIN_MENU_LIST.map((menu) => {
         return <AdminLink key={menu.id} path={menu.path} title={menu.title} />;
       })}
-    </Wrapper>
+    </Container>
   );
 }
--- front/src/app/_components/_header/Header.tsx
+++ front/src/app/_components/_header/Header.tsx
@@ -3,19 +3,19 @@ import { HeaderPropsType } from 'types';
 import { headerContainer, wrapper, headerLinkContainer } from './header.css';
 import HeaderLinkList from '../_composables/headerLinks/HeaderLinkList';
 import HeaderBanner from './HeaderBanner';
-import Wrapper from '../_composables/wrapper/Wrapper';
+import Container from '../_composables/container/Container';
 import Admin from '../_admin/Admin';
 
 export default function Header({ isAdmin }: HeaderPropsType) {
   return (
     <div className={`${headerContainer} ${myStyle}`}>
-      <Wrapper className={wrapper}>
+      <Container className={wrapper}>
         <HeaderBanner />
-        <Wrapper className={headerLinkContainer}>
+        <Container className={headerLinkContainer}>
           {isAdmin && <Admin />}
           <HeaderLinkList />
-        </Wrapper>
-      </Wrapper>
+        </Container>
+      </Container>
     </div>
   );
 }
