47265bd6a625b712d4a9bd2c318772da0a635b54
brgndy <brgndy96@gmail.com>
2024-01-07 12:31:11 +0900

    Feat: 게시글 설명 컴포넌트 생성

---
+++ front/src/app/_components/_write/PostDescription.tsx
@@ -0,0 +1,6 @@
+import Container from '../_composables/container/Container';
+import { descriptionContainer } from './postDescription.css';
+
+export default function PostDescription() {
+  return <Container className={descriptionContainer}>PostDescription</Container>;
+}
