0d51d9ac5cfb85f556dcc158c4d3f329420a271b
brgndy <brgndy96@gmail.com>
2024-01-07 12:30:38 +0900

    Feat: 썸네일 이미지 컴포넌트 생성

---
+++ front/src/app/_components/_write/ThumbnailInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Container from '../_composables/container/Container';
+import { thumbnailInputContainer } from './thumbnailInput.css';
+
+export default function ThumbnailInput() {
+  return <Container className={thumbnailInputContainer}>ThumbnailImage</Container>;
+}
