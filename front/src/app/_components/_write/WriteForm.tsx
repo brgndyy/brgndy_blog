31b1d334c0f11c33e984ad464339b294ef549a32
brgndy <brgndy96@gmail.com>
2024-01-07 12:31:58 +0900

    Feat: 썸네일, 게시글 설명 컴포넌트 추가

--- front/src/app/_components/_write/WriteForm.tsx
+++ front/src/app/_components/_write/WriteForm.tsx
@@ -1,13 +1,29 @@
+'use client';
+
+import { useState } from 'react';
 import ContentSection from './ContentSection';
 import TitleSection from './TitleSection';
 import ButtonSection from './ButtonSection ';
+import TotalSubmitForm from './TotalSubmitForm';
 
 export default function WriteForm() {
+  const [isOpenSubmitForm, setIsOpenSubmitForm] = useState(false);
+
+  const openSubmitFormHandler = () => {
+    setIsOpenSubmitForm(!isOpenSubmitForm);
+  };
+
   return (
     <>
       <TitleSection />
       <ContentSection />
-      <ButtonSection />
+      <ButtonSection openSubmitFormHandler={openSubmitFormHandler} />
+      {isOpenSubmitForm && (
+        <TotalSubmitForm
+          isOpenSubmitForm={isOpenSubmitForm}
+          openSubmitFormHandler={openSubmitFormHandler}
+        />
+      )}
     </>
   );
 }
