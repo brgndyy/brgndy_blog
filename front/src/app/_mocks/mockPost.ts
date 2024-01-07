3f35f39f19828c5b0d9dbb137d3c1e937bee3792
brgndy <brgndy96@gmail.com>
2024-01-07 14:28:31 +0900

    Feat: mock 게시글 생성

---
+++ front/src/app/_mocks/mockPost.ts
@@ -0,0 +1,22 @@
+const MOCK_POST = [
+  {
+    id: 1,
+    thumbnailImageSrc: '/images/메트로놈 라이브러리 컴포넌트 만들기.png',
+    title: '메트로놈 라이브러리 컴포넌트 만들기',
+    slug: '메트로놈-라이브러리-컴포넌트-만들기',
+    description: '나만의 라이브러리 컴포넌트 만들어보기',
+    body: `# 안녕하세요 `,
+    userInfo: {
+      id: 1,
+      userId: 'brgndy',
+      userName: '버건디',
+      isAdmin: true,
+      createdAt: '2024-01-04T00:52:58.600Z',
+      updatedAt: '2024-01-04T00:52:58.600Z',
+    },
+    createdAt: '2024-01-04T00:52:58.600Z',
+    updatedAt: '2024-01-04T00:52:58.600Z',
+  },
+];
+
+export default MOCK_POST;
