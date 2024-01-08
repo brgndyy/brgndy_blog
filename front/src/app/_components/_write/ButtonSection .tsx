bcb54a2f610ea18229725c447abc76b1ddad628a
brgndy <brgndy96@gmail.com>
2024-01-08 16:43:56 +0900

    Feat: 게시글 작성 컴포넌트 각 섹션으로 분리

--- front/src/app/_components/_write/ButtonSection .tsx
+++ front/src/app/_components/_write/ButtonSection .tsx
@@ -1,14 +1,19 @@
 import { BMHANNAAir } from '@/app/_styles/fonts/fonts';
 import { myStyle } from '@/app/_styles/vars.css';
+import { ButtonSelectionPropsType } from 'types';
 import Container from '../_composables/container/Container';
 import { allButtonConatiner, button } from './buttonSelection.css';
 import Button from '../_composables/button/Button';
 
-export default function ButtonSection() {
+export default function ButtonSection({ openSubmitFormHandler }: ButtonSelectionPropsType) {
   return (
     <Container className={allButtonConatiner}>
       <Button className={`${button} ${BMHANNAAir.className} ${myStyle}`} text="나가기" />
-      <Button className={`${button} ${BMHANNAAir.className} ${myStyle}`} text="작성하기" />
+      <Button
+        className={`${button} ${BMHANNAAir.className} ${myStyle}`}
+        text="작성하기"
+        onClick={openSubmitFormHandler}
+      />
     </Container>
   );
 }
--- front/src/app/_components/_write/TitleSection.tsx
+++ front/src/app/_components/_write/TitleSection.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { myStyle } from '@/app/_styles/vars.css';
+import { BMHAANAPro } from '@/app/_styles/fonts/fonts';
+import Container from '../_composables/container/Container';
+import { titleContainer, titleInput } from './titleSection.css';
+import Input from '../_composables/input/Input';
 
 export default function TitleSection() {
-  return <div>TitleSection</div>;
+  return (
+    <Container className={titleContainer}>
+      <Input
+        type="text"
+        placeholder="제목을 입력하세요"
+        className={`${titleInput} ${BMHAANAPro.className} ${myStyle}`}
+        onChange={() => {}}
+      />
+    </Container>
+  );
 }
