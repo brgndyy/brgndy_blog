ddb3c5c9baaf70aba7fe5a8aab9dd796fb4ce65a
brgndy <brgndy96@gmail.com>
2024-01-08 14:57:09 +0900

    Refacotr: 헤더 배너 수정

--- front/src/app/_components/_header/HeaderBanner.tsx
+++ front/src/app/_components/_header/HeaderBanner.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import { myStyle } from '@/app/_styles/vars.css';
 import { BMHANNAAir } from '@/app/_styles/fonts/fonts';
-import { headerBanner, bannerText } from './headerBanner.css';
+import { headerBanner, banner } from './headerBanner.css';
 
 export default function HeaderBanner() {
   return (
-    <Link href="/" className={bannerText}>
+    <Link href="/" className={banner}>
       <h1 className={`${BMHANNAAir.className} ${myStyle} ${headerBanner}`}>BRGNDY의 개발 블로그</h1>
     </Link>
   );
--- front/src/app/_components/_header/headerBanner.css.ts
+++ front/src/app/_components/_header/headerBanner.css.ts
@@ -3,6 +3,12 @@ import { text1 } from '@/app/_styles/vars.css';
 
 export const bannerText = style({});
 
+export const banner = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
 export const headerBanner = style({
   color: text1,
   margin: '0 1rem',
