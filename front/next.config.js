a44ac6cdb6048c7ea9fe7a9381597806debfbe41
brgndy <brgndy96@gmail.com>
2024-01-09 16:01:27 +0900

    Chore: next.config.js, package.json 수정

--- front/next.config.js
+++ front/next.config.js
@@ -1,9 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3002',
+      },
+    ],
+  },
+};
 
 module.exports = withVanillaExtract(nextConfig);
