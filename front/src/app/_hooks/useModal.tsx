a032f51b3bbabc5f12ad5dfca2a730086d6d7253
brgndy <brgndy96@gmail.com>
2024-01-13 12:48:49 +0900

    Refactor: useModal 훅 리팩토링

--- front/src/app/_hooks/useModal.tsx
+++ front/src/app/_hooks/useModal.tsx
@@ -1,19 +1,33 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const useModal = () => {
+const useModal = (animationTime?: number) => {
+  const animationTimer = animationTime || 300;
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalClosing, setIsModalClosing] = useState(false);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+
+    if (isModalClosing) {
+      timer = setTimeout(() => {
+        setIsModalOpen(false);
+        setIsModalClosing(false);
+      }, animationTimer);
+    }
+    return () => clearTimeout(timer);
+  }, [isModalClosing, animationTimer]);
 
   const modalOpenHandler = () => {
     setIsModalOpen(true);
   };
 
   const modalCloseHandler = () => {
-    setIsModalOpen(false);
+    setIsModalClosing(true); // 먼저 애니메이션 시작
   };
 
-  return { isModalOpen, setIsModalOpen, modalOpenHandler, modalCloseHandler };
+  return { isModalOpen, setIsModalOpen, modalOpenHandler, modalCloseHandler, isModalClosing };
 };
 
 export default useModal;
