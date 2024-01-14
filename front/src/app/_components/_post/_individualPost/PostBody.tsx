761053c407c9f644ceec10171deb6188f543bd08
brgndy <brgndy96@gmail.com>
2024-01-14 11:13:20 +0900

    Refactor: 게시글에 sanitize 속성 추가

--- front/src/app/_components/_post/_individualPost/PostBody.tsx
+++ front/src/app/_components/_post/_individualPost/PostBody.tsx
@@ -5,15 +5,18 @@ import MarkdownPreview from '@uiw/react-markdown-preview';
 import { BMHANNAAir } from '@/app/_styles/fonts/fonts';
 import { PostBodyPropsType } from 'types';
 import formatMarkdownBodyContent from '@/app/_utils/formatMarkdownString';
+import rehypeSanitize from 'rehype-sanitize';
 import { bodyText } from './postBody.css';
 
 export default function PostBody({ body }: PostBodyPropsType) {
   const formattedMarkDownBodyContent = formatMarkdownBodyContent(body);
+  const rehypePlugins = [rehypeSanitize];
 
   return (
     <MarkdownPreview
       source={formattedMarkDownBodyContent}
       className={`${BMHANNAAir.className} ${bodyText}`}
+      rehypePlugins={rehypePlugins}
     />
   );
 }
