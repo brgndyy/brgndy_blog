0ab23cb429b4f62de32053da807f803edccd410f
brgndy <brgndy96@gmail.com>
2024-01-17 15:01:46 +0900

    Feat: 게시글마다 메타데이터 추가

--- front/src/app/posts/[slug]/page.tsx
+++ front/src/app/posts/[slug]/page.tsx
@@ -3,6 +3,22 @@ import getIndividualPost from '@/app/_services/getIndividualPost';
 import IndividualPost from '@/app/_components/_post/_individualPost/IndividualPost';
 import getAccessTokenValue from '@/app/_services/getAccessTokenValue';
 import getUserInfoByAccessToken from '@/app/_services/getUserInfoByAccessToken';
+import type { Metadata } from 'next';
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = params;
+  const decodedSlug = decodeURIComponent(slug);
+  const post = await getIndividualPost(decodedSlug);
+
+  return {
+    title: post.post.title,
+    description: post.post.description || '게시글에 대한 설명',
+  };
+}
 
 export default async function PostPage({ params }: { params: { slug: string } }) {
   const accessToken = getAccessTokenValue();
