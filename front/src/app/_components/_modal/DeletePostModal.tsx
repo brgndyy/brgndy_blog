4ddd53af2865e6ffc56ad34ae7868e649274e749
brgndy <brgndy96@gmail.com>
2024-01-17 12:41:02 +0900

    Feat: 프론트 게시글 삭제 기능 구현

--- front/src/app/_components/_modal/DeletePostModal.tsx
+++ front/src/app/_components/_modal/DeletePostModal.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
 import { BMHANNAAir } from '@/app/_styles/fonts/fonts';
 import { myStyle } from '@/app/_styles/vars.css';
+import useFetch from '@/app/_hooks/useFetch';
+import ERROR_MESSAGE from '@/app/_constants/errorMessage';
+import PATH_ROUTES from '@/app/_constants/pathRoutes';
+import { useParams, useRouter } from 'next/navigation';
 import { closeButton } from './authModal.css';
-import {
-  closeModalAnimation,
-  modalContainer,
-  modalText,
-  deleteButton,
-} from './deletePostModal.css';
+import { modalContainer, modalText, deleteButton } from './deletePostModal.css';
 import Modal from './Modal';
 import Container from '../_composables/container/Container';
 import Button from '../_composables/button/Button';
 
 export default function DeletePostModal() {
+  const { sendRequest } = useFetch();
+  const { slug } = useParams<{ slug: string }>();
+  const decodedURIComponent = decodeURIComponent(slug);
+  const router = useRouter();
+
+  const deletePostHandler = async () => {
+    try {
+      const res = await sendRequest(
+        `${process.env.NEXT_PUBLIC_DEFAULT_BACKEND_URL}${PATH_ROUTES.delete_post(
+          decodedURIComponent,
+        )}`,
+        null,
+        {},
+        'DELETE',
+      );
+
+      const data = await res.json();
+
+      const { success } = data;
+
+      if (success) {
+        router.replace('/');
+      }
+    } catch (err) {
+      throw new Error(ERROR_MESSAGE.fail_delete_post);
+    }
+  };
+
   return (
     <Modal.Portal id="modal">
       <Modal.Backdrop>
-        <Modal.Container
-          className={`${modalContainer} ${BMHANNAAir.className} ${
-            // isModalClosing ? closeModalAnimation : ''
-            ''
-          }`}
-        >
+        <Modal.Container className={`${modalContainer} ${BMHANNAAir.className}`}>
           <Modal.Close className={`${closeButton} ${myStyle} ${BMHANNAAir.className}`}>
             X
           </Modal.Close>
@@ -34,6 +56,7 @@ export default function DeletePostModal() {
             <Button
               text="삭제하기"
               className={`${myStyle} ${BMHANNAAir.className} ${deleteButton}`}
+              onClick={deletePostHandler}
             />
           </Container>
         </Modal.Container>
