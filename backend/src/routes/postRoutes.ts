5444ba61db8d6b359d536ba12f0ba090828dccf4
brgndy <brgndy96@gmail.com>
2024-01-17 13:44:06 +0900

    Refactor: 필요없는 라우트 삭제

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -3,7 +3,6 @@ import getAllPosts from '../controllers/getAllPosts';
 import getIndividualPostBySlug from '../controllers/getIndividualPostBySlug';
 import generateNewPost from '../controllers/generateNewPost';
 import postThumbnailImageUpload from '../middlewares/postThumbnailImageUpload';
-import sendPostThumbnailImage from '../controllers/sendPostThumbnailImage';
 import deletePostBySlug from '../controllers/deletePostBySlug';
 
 const router = Router();
@@ -11,7 +10,6 @@ const router = Router();
 router.get('/:slug', getIndividualPostBySlug);
 router.get('', getAllPosts);
 router.post('', postThumbnailImageUpload.single('thumbnailImage'), generateNewPost);
-router.post('/thumbnail-image', sendPostThumbnailImage);
 router.delete('/:slug', deletePostBySlug);
 
 export { router as postRoutes };
