07228d8165e4390d34a0d728b51f3409183c846b
brgndy <brgndy96@gmail.com>
2024-01-17 15:02:27 +0900

    Style: favicon 설정

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -9,6 +9,9 @@ import getUserInfoByAccessToken from './_services/getUserInfoByAccessToken';
 export const metadata: Metadata = {
   title: 'BRGNDY의 개발 블로그',
   description: '안녕하세요. 개발 공부를 하며 내용을 기재해놓는 철저히 개인적인 블로그입니다.',
+  icons: {
+    icon: '/faviconImage.ico',
+  },
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
