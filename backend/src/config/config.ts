5ca93395f9c99353bf1c82c81ec19e489b5d995f
brgndy <brgndy96@gmail.com>
2024-01-26 18:15:00 +0900

    Refactor: 배포 환경에 맞게 config 설정

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,7 +6,7 @@ const config = {
     username: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE || 'dataschema',
-    host: '127.0.0.1',
+    host: process.env.MYSQL_HOST || '127.0.0.1',
     dialect: 'mysql',
   },
   test: {
@@ -20,8 +20,9 @@ const config = {
     username: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE || 'dataschema',
-    host: 'mysql',
+    host: process.env.MYSQL_HOST || '127.0.0.1',
     dialect: 'mysql',
+    port: 3306,
   },
 };
 
