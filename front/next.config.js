79e0e64ac088f68c1c3b3734492df856653ad30c
brgndy <brgndy96@gmail.com>
2024-01-28 19:16:08 +0900

    Chore: add next.config.js standalone property

--- front/next.config.js
+++ front/next.config.js
@@ -5,6 +5,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
