6867ad77389ce3489addc11209bcbab9fdd1c400
brgndy <brgndy96@gmail.com>
2024-01-30 14:26:04 +0900

    Test: 에러 디버깅용

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,12 +3,12 @@ import getAllPosts from './_services/getAllPosts';
 import AuthModalTrigger from './_components/_auth/AuthModalTrigger';
 
 export default async function HomePage() {
-  const allPosts = await getAllPosts();
-  const { allPost } = allPosts;
+  // const allPosts = await getAllPosts();
+  // const { allPost } = allPosts;
 
   return (
     <>
-      <AllPost allPosts={allPost} />
+      {/* <AllPost allPosts={allPost} /> */}
       <AuthModalTrigger />
     </>
   );
