83c1949cfe869fb17108cef2517407df93e9a272
brgndy <brgndy96@gmail.com>
2024-01-30 14:30:44 +0900

    Test: 디버깅용 재배포

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,14 +1,8 @@
-import AllPost from './_components/_post/AllPost';
-import getAllPosts from './_services/getAllPosts';
 import AuthModalTrigger from './_components/_auth/AuthModalTrigger';
 
 export default async function HomePage() {
-  // const allPosts = await getAllPosts();
-  // const { allPost } = allPosts;
-
   return (
     <>
-      {/* <AllPost allPosts={allPost} /> */}
       <AuthModalTrigger />
     </>
   );
