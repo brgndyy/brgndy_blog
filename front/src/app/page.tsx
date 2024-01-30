bfe8c16a761a3f28db0587d7f3064dcf0eabc0e9
brgndy <brgndy96@gmail.com>
2024-01-30 14:34:50 +0900

    Test: 디버깅용 재배포

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,5 @@
 import AuthModalTrigger from './_components/_auth/AuthModalTrigger';
 
 export default async function HomePage() {
-  return (
-    <>
-      <AuthModalTrigger />
-    </>
-  );
+  return <AuthModalTrigger />;
 }
