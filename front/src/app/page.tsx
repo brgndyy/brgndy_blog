5f0332a29c215fcb4f59461fe2c4473c253c1491
brgndy <brgndy96@gmail.com>
2024-01-30 14:56:38 +0900

    Test: 디버깅용 재배포

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,5 +1,15 @@
+import AllPost from './_components/_post/AllPost';
+import getAllPosts from './_services/getAllPosts';
 import AuthModalTrigger from './_components/_auth/AuthModalTrigger';
 
 export default async function HomePage() {
-  return <AuthModalTrigger />;
+  const allPosts = await getAllPosts();
+  const { allPost } = allPosts;
+
+  return (
+    <>
+      <AllPost allPosts={allPost} />
+      <AuthModalTrigger />
+    </>
+  );
 }
