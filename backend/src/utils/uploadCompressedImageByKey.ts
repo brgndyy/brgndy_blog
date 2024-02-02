4fbaa886b6372bad601994b4080247ea63031a81
brgndy <brgndy96@gmail.com>
2024-02-02 14:07:56 +0900

    Refactor: 이미지 변경

--- backend/src/utils/uploadCompressedImageByKey.ts
+++ backend/src/utils/uploadCompressedImageByKey.ts
@@ -6,6 +6,7 @@ import {
   DeleteObjectCommand,
 } from '@aws-sdk/client-s3';
 import sharp = require('sharp');
+import { Readable } from 'nodemailer/lib/xoauth2';
 
 dotenv.config();
 
@@ -22,6 +23,20 @@ const s3 = new S3Client({
   },
 });
 
+const streamToBuffer = async (stream: Readable | ReadableStream | Blob): Promise<Buffer> => {
+  if (stream instanceof Readable) {
+    // Node.js Readable 스트림인 경우
+    return new Promise((resolve, reject) => {
+      const chunks: Buffer[] = [];
+      stream.on('data', (chunk) => chunks.push(chunk));
+      stream.on('end', () => resolve(Buffer.concat(chunks)));
+      stream.on('error', reject);
+    });
+  } else {
+    throw new Error('스트림타입이 일차하지 않아요');
+  }
+};
+
 const uploadCompressedImageByKey = async (key: string) => {
   try {
     const compressedKey = `compressed_${key}`;
@@ -31,30 +46,33 @@ const uploadCompressedImageByKey = async (key: string) => {
       Key: key,
     };
 
-    let resizedConfig: any = {
-      Bucket: bucketName,
-      Key: compressedKey,
-    };
+    // S3에서 이미지 불러오기
+    const { Body } = await s3.send(new GetObjectCommand(config));
+    if (!Body) throw new Error('S3로부터 이미지를 가져오는데에 실패했어요!');
 
-    //기존 이미지 불러오기
-    const imageData: any = await s3.send(new GetObjectCommand(config));
+    // 스트림을 버퍼로 변환
+    const buffer = await streamToBuffer(Body);
 
     // 이미지 리사이징
-    const imageBuffer = await sharp(imageData.Body)
-      .resize({ height: 1920, width: 1080 })
-      .toBuffer();
-
-    resizedConfig.Body = imageBuffer;
+    const imageBuffer = await sharp(buffer).resize({ height: 1920, width: 1080 }).toBuffer();
 
     // 리사이징한 이미지 업로드
-    await s3.send(new PutObjectCommand(resizedConfig));
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: compressedKey,
+        Body: imageBuffer,
+        ContentType: 'image/jpeg',
+      }),
+    );
 
-    // 기존에 있던 이미지는 삭제
+    // 기존 이미지 삭제
     await s3.send(new DeleteObjectCommand(config));
 
     return compressedKey;
   } catch (error) {
-    console.log('Get image by key from aws: ', error);
+    console.error('이미지를 찾는데에 에러가 발생한 key', error);
+    throw error; // 오류를 더 상위로 전파
   }
 };
 
