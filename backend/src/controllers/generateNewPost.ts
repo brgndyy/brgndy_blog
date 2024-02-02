2ccaa9dac421b21d1d015547c8d9bd6bbaef9332
brgndy <brgndy96@gmail.com>
2024-02-02 10:23:47 +0900

    Refactor: multers3 가 아닌 일반 multer로 교체

--- backend/src/controllers/generateNewPost.ts
+++ backend/src/controllers/generateNewPost.ts
@@ -5,12 +5,42 @@ import formatThumbnailImagePath from '../services/post/formatThumbnailImagePath'
 import createNewPost from '../services/post/createNewPost';
 import isExistingPostByTitle from '../services/post/findExistingPostByTitle';
 import updateExistingPost from '../services/post/updateExistingPost';
+import { S3Client, ListObjectsCommand } from '@aws-sdk/client-s3';
 
 const generateNewPost = async (req: Request, res: Response, next: NextFunction) => {
   try {
     if (!req.file) {
       throw new HttpError(ERROR_MESSAGE.fail_create_new_post, 503);
     }
+
+    const bucketName = process.env.BUCKET_NAME || '';
+    const bucketRegion = process.env.BUCKET_REGION || '';
+    const accessKey = process.env.ACCESS_KEY || '';
+    const secretAccessKey = process.env.SECRET_ACCESS_KEY || '';
+
+    const s3 = new S3Client({
+      region: bucketRegion,
+      credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secretAccessKey,
+      },
+    });
+
+    const listObjects = async () => {
+      try {
+        const data = await s3.send(
+          new ListObjectsCommand({
+            Bucket: bucketName,
+          }),
+        );
+        console.log('Success!', data);
+      } catch (err) {
+        console.error('Error', err);
+      }
+    };
+
+    listObjects();
+
     const thumbnailImageSrc = formatThumbnailImagePath(req.file.path);
 
     console.log('req.file : ', req.file.path);
--- backend/src/middlewares/upload.ts
+++ backend/src/middlewares/upload.ts
@@ -2,56 +2,27 @@ import multer from 'multer';
 import fs from 'fs';
 import PATH from '../constants/path/path';
 import * as dotenv from 'dotenv';
-import { S3Client, ListObjectsCommand } from '@aws-sdk/client-s3';
-import multerS3 from 'multer-s3';
 
 dotenv.config();
 
-const bucketName = process.env.BUCKET_NAME || '';
-const bucketRegion = process.env.BUCKET_REGION || '';
-const accessKey = process.env.ACCESS_KEY || '';
-const secretAccessKey = process.env.SECRET_ACCESS_KEY || '';
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // 폴더가 존재하지 않는 경우에 폴더 생성
+    if (!fs.existsSync(PATH.image_url)) {
+      fs.mkdirSync(PATH.image_url, { recursive: true });
+    }
 
-console.log('bucketName :', bucketName);
-console.log('bucketRegion :', bucketRegion);
-console.log('accessKey :', accessKey);
-console.log('secretAccessKey :', secretAccessKey);
-
-const s3 = new S3Client({
-  region: bucketRegion,
-  credentials: {
-    accessKeyId: accessKey,
-    secretAccessKey: secretAccessKey,
+    cb(null, PATH.image_url);
+  },
+  filename: function (req, file, cb) {
+    // 띄어쓰기를 하이픈으로 대체
+    const decodedFilename = Buffer.from(file.originalname, 'latin1').toString('utf8');
+    // 띄어쓰기를 하이픈으로 대체
+    const safeFilename = decodedFilename.replace(/\s/g, '-');
+    cb(null, safeFilename);
   },
 });
 
-const listObjects = async () => {
-  try {
-    const data = await s3.send(
-      new ListObjectsCommand({
-        Bucket: bucketName,
-      }),
-    );
-    console.log('Success!', data);
-  } catch (err) {
-    console.error('Error', err);
-  }
-};
-
-listObjects();
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: bucketName,
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      const filename = `${Date.now().toString()}-${file.originalname}`;
-      cb(null, filename);
-    },
-  }),
-});
+const upload = multer({ storage: storage });
 
 export default upload;
