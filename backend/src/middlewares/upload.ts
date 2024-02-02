d745e699c1223e3beb70562308fdf62ec0c03831
brgndy <brgndy96@gmail.com>
2024-02-02 10:11:40 +0900

    Refactor: s3 디버깅

--- backend/src/middlewares/upload.ts
+++ backend/src/middlewares/upload.ts
@@ -12,6 +12,11 @@ const bucketRegion = process.env.BUCKET_REGION || '';
 const accessKey = process.env.ACCESS_KEY || '';
 const secretAccessKey = process.env.SECRET_ACCESS_KEY || '';
 
+console.log('bucketName :', bucketName);
+console.log('bucketRegion :', bucketRegion);
+console.log('accessKey :', accessKey);
+console.log('secretAccessKey :', secretAccessKey);
+
 const s3 = new S3Client({
   region: bucketRegion,
   credentials: {
