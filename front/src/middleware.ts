a624a27fff8e158e69d092b6fd9da701362be7f7
brgndy <brgndy96@gmail.com>
2024-02-09 22:50:44 +0900

    Refactor: 액세스토큰 미들웨어 조건 추가

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -7,18 +7,22 @@ import TOKEN_COOKIE_CONFIG from './app/_constants/tokenCookieConfig';
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const { accessToken, refreshToken } = getTokenValues(request);
+  const alreadyRedirected = request.cookies.get('alreadyRedirected');
 
-  if (!accessToken && refreshToken) {
+  if (!accessToken && refreshToken && !alreadyRedirected) {
     const newResponse = NextResponse.redirect(request.nextUrl.clone());
     const res = await getNewAccessToken(refreshToken);
-
-    console.log('res : ', res); // received well
 
     const { newAccessToken } = res;
 
     newResponse.cookies.set('accessToken', newAccessToken, TOKEN_COOKIE_CONFIG.access_token);
+    newResponse.cookies.set('alreadyRedirected', 'true', { path: '/', maxAge: 60 });
 
     return newResponse;
+  }
+
+  if (alreadyRedirected) {
+    response.cookies.delete('alreadyRedirected');
   }
 
   return response;
