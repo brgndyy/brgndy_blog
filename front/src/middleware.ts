106489f19dc5b3f70f426bb057131c936dcdc734
brgndy <brgndy96@gmail.com>
2024-02-16 22:50:43 +0900

    Refactor: 액세스토큰 쿠키 셋팅 옵션 제거

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -2,24 +2,19 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import getTokenValues from './app/_services/getTokenValue';
 import { getNewAccessToken } from './app/_services/getNewAccessToken';
-import TOKEN_COOKIE_CONFIG from './app/_constants/tokenCookieConfig';
 
 export async function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
   const { accessToken, refreshToken } = getTokenValues(request);
   const fetchMode = request.headers.get('sec-fetch-mode');
 
-  if (
-    (url.pathname === '/' && !accessToken && refreshToken) ||
-    (url.pathname === '/' && fetchMode === 'navigate' && refreshToken)
-  ) {
+  if ((!accessToken && refreshToken) || (fetchMode === 'navigate' && refreshToken)) {
     const res = await getNewAccessToken(refreshToken);
 
     if (res && res.newAccessToken) {
       const response = NextResponse.next();
-      const { newAccessToken } = res;
+      const { newAccessToken } = res; // it works wll
 
-      response.cookies.set('accessToken', newAccessToken, TOKEN_COOKIE_CONFIG.access_token);
+      response.cookies.set('accessToken', newAccessToken);
       return response;
     }
   }
