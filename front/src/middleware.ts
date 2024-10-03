65fd41de163b641846df3e46d57242605f5cf941
brgndy <brgndy96@gmail.com>
2024-10-03 20:01:28 +0900

    test: repush

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -14,7 +14,6 @@ export async function middleware(request: NextRequest) {
     if (res && res.newAccessToken) {
       const { newAccessToken } = res;
 
-      console.log('test!');
       response.cookies.set('accessToken', newAccessToken, {
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
       });
