6891d86793655f2879ae0ab21f863ca21c7f1ce2
brgndyy <brgndyy@users.noreply.github.com>
2026-08-16 22:01:53 +0900

    fix: return 404 for missing posts

--- front/src/app/posts/[slug]/page.tsx
+++ front/src/app/posts/[slug]/page.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import getIndividualPost from '@/app/_utils/getIndividualPost';
 import IndividualPost from '@/app/_components/_post/_individualPost/IndividualPost';
 import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
 type Props = {
   params: { slug: string };
 };
 
+async function getPost(slug: string) {
+  try {
+    return await getIndividualPost(decodeURIComponent(slug));
+  } catch {
+    return notFound();
+  }
+}
+
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug } = params;
-  const decodedSlug = decodeURIComponent(slug);
-  const post = await getIndividualPost(decodedSlug);
+  const post = await getPost(params.slug);
 
   return {
     title: `전태헌 개발 블로그의 게시글 제목: ${post.title}`,
@@ -18,10 +25,8 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export default async function PostPage({ params }: { params: { slug: string } }) {
-  const { slug } = params;
-  const decodedSlug = decodeURIComponent(slug);
-  const post = await getIndividualPost(decodedSlug);
+export default async function PostPage({ params }: Props) {
+  const post = await getPost(params.slug);
 
   return <IndividualPost title={post.title} content={post.content} date={post.date} />;
 }
